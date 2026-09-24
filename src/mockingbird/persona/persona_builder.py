c08dca55a78501abdd1b369345cb3ac75693c580
Silazemli <you@example.com>
2026-09-24 16:38:59 +0300

    major refactoring

--- src/mockingbird/persona/persona_builder.py
+++ src/mockingbird/persona/persona_builder.py
@@ -1,55 +1,56 @@
+import numpy as np
+
 from mockingbird.analysis.stats import (
-    avg_message_len_by_user,
-    burst_len_by_user,
-    distinctive_ngrams,
-    emoji_frequency_by_user,
-    most_common_emojis_by_user,
-    ngram_frequency_by_user,
-    response_latency_by_user,
-    stat_burst_user,
-    stat_response_latency_by_user,
+    avg_message_len,
+    message_bursts,
+    emoji_frequency,
+    response_latencies,
+    most_common_emojis,
 )
-from mockingbird.datacls import Message
-from mockingbird.persona.model import (
+from mockingbird.datacls import (
     BehavioralPatterns,
     CommunicationStyle,
+    Conversation,
+    EmojiUse,
+    Lexicon,
+    MessagingPatterns,
     Persona,
 )
 
 
-def build_persona(user_id: int, messages: list[Message]):
+def build_persona(conversations: list[Conversation]) -> Persona:
+    persona = Persona()
 
-    other_user_id = next(
-        uid for uid in set(m.user_id for m in messages) if uid != user_id
-    )
+    for conversation in conversations:
+        messages = conversation.messages
 
-    avg_message_len = avg_message_len_by_user(messages=messages)[user_id]
-    emoji_frequency = emoji_frequency_by_user(messages=messages)[user_id]
-    most_common_emojis = most_common_emojis_by_user(messages=messages)[user_id]
-    burst_stats = stat_burst_user(burst_len_by_user(messages))[user_id]
-    response_latency_stats = stat_response_latency_by_user(
-        response_latency_by_user(messages)
-    )[user_id]
+        if not messages:
+            continue
 
-    freq = ngram_frequency_by_user(messages, n=1)
-    distinctive_words = distinctive_ngrams(freq[user_id], freq[other_user_id])
+        persona_messages = [message for message in messages if not message.from_other]
 
-    freq2 = ngram_frequency_by_user(messages, n=2)
-    distinctive_phrases = distinctive_ngrams(freq2[user_id], freq2[other_user_id])
+        bursts = message_bursts(persona_messages)
+        latencies = response_latencies(persona_messages)
 
-    style = CommunicationStyle(
-        avg_message_len=avg_message_len,
-        emoji_frequency=emoji_frequency,
-        most_common_emojis=most_common_emojis,
-        burst_stats=burst_stats,
-        response_latency_stats=response_latency_stats,
-        distinctive_words=distinctive_words,
-        distinctive_phrases=distinctive_phrases,
-        sample_messages=["hi"],
-    )
+        messaging_patterns = MessagingPatterns(
+            np.max(bursts),
+            np.mean(bursts),
+            np.median(latencies),
+            np.mean(latencies),
+        )
 
-    return Persona(
-        user_id=user_id,
-        message_count=sum(1 for m in messages if m.user_id == user_id),
-        communication_style=style,
-    )
+        emoji_use = EmojiUse(
+            emoji_frequency(persona_messages), most_common_emojis(persona_messages)
+        )
+
+        lexicon = Lexicon([], [])  # idk about that honestly, fix pls
+
+        persona.communication_styles[conversation.user] = CommunicationStyle(
+            avg_message_len(persona_messages),
+            emoji_use,
+            messaging_patterns,
+            lexicon,
+            [],  # TODO
+        )
+
+    persona.behavioral_patterns = BehavioralPatterns([])  # TODO
